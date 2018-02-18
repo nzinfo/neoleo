3dd0f9fe06a3d166ba24a322cc45c5f916b2522c
Mark Carter <alt.mcarter@gmail.com>
2018-02-18 18:59:10 +0000

    modified:   formula-parser.scm

--- src/formula-parser.scm
+++ src/formula-parser.scm
@@ -1,6 +1,20 @@
 (use-modules (ice-9 peg string-peg))
+(use-modules (ice-9 peg using-parsers))
+(use-modules (ice-9 pretty-print))
+(use-modules (ice-9 format))
+
 
 (define-peg-string-patterns
-  "passwd <-entry* !.
-  entry <-- (! NL .)* NL*
-  NL < '\n'")
+  "formula <-- func / num
+  func <-- funct '(' num ')'
+  funct <-- [a-z]+
+  num <-- [0-9]+
+  ")
+
+
+(define (show str)
+  (format #t "\nParsing: ~s\n" str)
+  (pretty-print (peg:tree (match-pattern formula str))))
+
+(show "hello(12)")
+(show "42")
